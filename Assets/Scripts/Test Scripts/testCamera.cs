63f48394af4ba4c6c53351952c500abfeaf9627c
Jonathan August Michelsen <jonathan.99@live.no>
2020-03-05 20:29:32 +0100

    Test Scene JAM
    Added a test scene for user JAM. Fiddling around with the core mechanics. Made a test player script with auto movement and jump implemented. As well as a camera follow scrip. Attempting to work at sliding and enemies. All of it is labeled test as we're still not sure which method we want to go for.

---
+++ Assets/Scripts/Test Scripts/testCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testCamera : MonoBehaviour
+{
+    public testPlayercontroller thePlayer;
+
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+    private void Start()
+    {
+        thePlayer = FindObjectOfType<testPlayercontroller>();
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+
+    private void Update()
+    {
+        distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;
+
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+}
---
+++ Assets/Scripts/Test Scripts/testPlayercontroller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testPlayercontroller : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody;
+
+    private BoxCollider2D myCollider;
+
+    public bool onPlatform = false;
+
+    public bool isSliding = false;
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+       
+    }
+
+    private void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        if(Input.touchCount > 0)
+        {
+            if(Input.GetTouch (0).phase == TouchPhase.Began)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
+        }
+
+        //Mouse and Keyboard Controller for testing purposes
+        if (Input.GetKeyDown(KeyCode.Space) && onPlatform == true)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && onPlatform == true)
+        {
+            transform.localScale = new Vector3(0.5f, 0.4f, 1);
+        }
+    }
+}
