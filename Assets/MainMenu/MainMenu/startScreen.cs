c903a4edaf6fed7413daf01a7927a63a98e69e12
Adelén Johansen <adelenjohansen1998@gmail.com>
2020-04-29 19:27:11 +0200

    added main menu

---
+++ Assets/MainMenu/MainMenu/startScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startScreen : MonoBehaviour
+{
+    public void AlphaScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+        
+}
